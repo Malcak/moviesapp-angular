a11c001f3566968cba94332c42047b4570aa1eab
Malcak <Malcakx@gmail.com>
2020-11-26 06:08:47 -0500

    feat: created movies poster grid

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SliderComponent } from './slider/slider.component';
+import { MoviesPosterGridComponent } from './movies-poster-grid/movies-poster-grid.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
-  declarations: [NavbarComponent, SliderComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SliderComponent],
+  declarations: [NavbarComponent, SliderComponent, MoviesPosterGridComponent],
+  imports: [CommonModule, RouterModule, RatingModule],
+  exports: [NavbarComponent, SliderComponent, MoviesPosterGridComponent],
 })
 export class ComponentsModule {}
---
+++ src/app/components/movies-poster-grid/movies-poster-grid.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/billboard';
+
+@Component({
+  selector: 'app-movies-poster-grid',
+  templateUrl: './movies-poster-grid.component.html',
+  styleUrls: ['./movies-poster-grid.component.scss'],
+})
+export class MoviesPosterGridComponent implements OnInit {
+  @Input() movies: Movie[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.movies);
+  }
+}
