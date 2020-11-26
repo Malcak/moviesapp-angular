879658b9002c288f84de654bf25671fad8887eef
Malcak <Malcakx@gmail.com>
2020-11-26 06:45:21 -0500

    feat: added infinite scroll on movies poster grid

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/billboard';
 import { MoviesService } from 'src/app/services/movies.service';
 
@@ -9,12 +9,34 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class HomeComponent implements OnInit {
   public billboard: Movie[] = [];
+  public movies: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const position =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const max =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (position > max) {
+      if (!this.movieService.downloading) {
+        this.movieService.getBillboard().subscribe((resp) => {
+          this.movies.push(...resp);
+        });
+      }
+    }
+  }
 
   constructor(private movieService: MoviesService) {
     this.movieService.getBillboard().subscribe((resp) => {
-      this.billboard = resp.results;
+      this.billboard = resp;
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.movieService.getBillboard().subscribe((resp) => {
+      this.billboard = resp;
+      this.movies = resp;
+    });
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,17 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Billboard } from '../models/billboard';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Billboard, Movie } from '../models/billboard';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  private baseUrl = 'https://api.themoviedb.org/3';
+  private billboardPage = 1;
+  public downloading = false;
+
   constructor(private http: HttpClient) {}
 
-  getBillboard(): Observable<Billboard> {
-    return this.http.get<Billboard>(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=37a083efdefcd3663056e6c88ce1d80e&language=es-Es&page=1'
-    );
+  get params(): {} {
+    return {
+      api_key: '37a083efdefcd3663056e6c88ce1d80e',
+      language: 'es-Es',
+      page: this.billboardPage.toString(),
+    };
+  }
+
+  getBillboard(): Observable<Movie[]> {
+    // if (this.downloading) {
+    //   return of([]);
+    // }
+    this.downloading = true;
+    return this.http
+      .get<Billboard>(`${this.baseUrl}/movie/now_playing?`, {
+        params: this.params,
+      })
+      .pipe(
+        map((resp) => resp.results),
+        tap(() => {
+          this.billboardPage += 1;
+          this.downloading = false;
+        })
+      );
   }
 }
