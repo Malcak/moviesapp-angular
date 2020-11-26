edd1c9e9035a791e9b4b7ca66b47918a6babbf8b
Malcak <Malcakx@gmail.com>
2020-11-26 05:35:30 -0500

    feat: improved slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/billboard';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent implements OnInit {
-  @Input()
-  movies: Movie[] = [];
+export class SliderComponent implements OnInit, AfterViewInit {
+  @Input() movies: Movie[] = [];
+  public mySwiper: Swiper | undefined;
 
   constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.movies);
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+    });
+  }
+
+  onSlideNext(): void {
+    this.mySwiper?.slideNext();
+  }
+  onSlidePrev(): void {
+    this.mySwiper?.slidePrev();
   }
 }
