d830c6232c358c0ed39efcbe730eeb181da992d5
Malcak <Malcakx@gmail.com>
2020-11-26 04:23:11 -0500

    feat: added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent,
+  },
+  {
+    path: 'movies/:filter',
+    component: MoviesComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
